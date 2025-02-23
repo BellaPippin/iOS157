cbdffe40e0c118d087500e0bc3a2949cb7d6f368
Maria C <mcosenza@students.mchenry.edu>
2025-02-23 13:18:03 -0600

    Switch app

--- carOptionsMariaC/carOptionsMariaC/ViewController.swift
+++ carOptionsMariaC/carOptionsMariaC/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var sliderLabel: UILabel!
+    
+    @IBOutlet weak var speakersLabel: UILabel!
+    
+    @IBOutlet weak var selectColorLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func sliderAwesomeness(_ sender: UISlider) {
+        sliderLabel.text = "Awesomeness: \(String(sender.value))"
+        
+    }
+    
+    @IBAction func sunRoof(_ sender: UISwitch) {
+        if sender.isOn {
+            let sunRoof = true
+        }
+        
+    }
+    
+    @IBAction func speakersStepper(_ sender: UIStepper) {
+        speakersLabel.text = String(Int(sender.value))
+    }
+    
+    @IBAction func selectcolor(_ sender: UIMenu) {
+        selectColorLabel.setTitle(sender.title, for: .normal)
+    }
+    
+    
 }
 
---
+++ switchLabMariaC/switchLabMariaC/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MyPageViewController.swift
+//  switchLabMariaC
+//
+//  Created by student on 2/20/25.
+//
+
+import UIKit
+
+class MyPageViewController: UIPageViewController {
+    // Your custom code for this page view controller
+}
+
