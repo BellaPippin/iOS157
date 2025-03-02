80e2c1319b2ca4407b6e0511e0c7b40fc3b4a73c
Maria C <mcosenza@students.mchenry.edu>
2025-03-02 15:50:49 -0600

    Week6

--- alertControllerTextFieldAppMariaC/alertControllerTextFieldAppMariaC/ViewController.swift
+++ alertControllerTextFieldAppMariaC/alertControllerTextFieldAppMariaC/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelResult.numberOfLines = 0
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        let alert = UIAlertController(title: "Guessing Password", message: "Enter Password", preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: {(textField) in
+            textField.placeholder = "Enter password..."
+            textField.isSecureTextEntry = true
+        })
+        
+        let okAction = UIAlertAction(title: "Go", style: .default, handler: {action -> Void in
+            let savedText = alert.textFields![0] as UITextField
+            self.labelResult.text = savedText.text
+        })
+        
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
