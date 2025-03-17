e959f5c7100c3c5277cbda8357be3819fa7f1056
Maria C <mcosenza@students.mchenry.edu>
2025-03-16 20:02:50 -0500

    What am I doing

--- myPListAppMariaC/myPListAppMariaC/ViewController.swift
+++ myPListAppMariaC/myPListAppMariaC/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var mySwitch: UISwitch!
+    
+    
+    @IBOutlet weak var mySlider: UISlider!
+    
+    
+    @IBOutlet weak var myTextField: UITextField!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
+        
+        statusLabel.text = " "
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveButton(_ sender: UIButton) {
+        UserDefaults.standard.set(mySwitch.isOn, forKey: "Switch")
+        UserDefaults.standard.set(mySlider.value, forKey: "Slider")
+        UserDefaults.standard.set(myTextField.text, forKey: "Text")
+        
+        statusLabel.text = "Saved!"
+    }
+    
+    @IBAction func loadButton(_ sender: UIButton) {
+        mySwitch.isOn = UserDefaults.standard.bool(forKey: "Switch")
+        mySlider.value = UserDefaults.standard.float(forKey: "Slider")
+        myTextField.text = UserDefaults.standard.string(forKey: "Text")
+        
+        statusLabel.text = "Loaded!"
+    }
+    
+    @IBAction func restoreButton(_ sender: UIButton) {
+        mySwitch.isOn = false
+        mySlider.value = 5.0
+        myTextField.text = " "
+        myTextField.placeholder = "MariaC"
+        
+        statusLabel.text = "Restored!"
+    }
+    
 }
 
---
+++ myTextFileAppMariaC/myTextFileAppMariaC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myTextFileAppMariaC
+//
+//  Created by student on 3/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
